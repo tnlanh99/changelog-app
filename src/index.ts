1adc41d15c9c8d220f35cdd22125ca70475432e1
tnlanh99 <tnlanh99@gmail.com>
2022-11-10 11:39:00 +0700

    Install morgan and body-parser middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import parser from "body-parser";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(parser.json());
 
 app.get("/", async (req, res) => {
   res.json({ message: "Hello, world" });
